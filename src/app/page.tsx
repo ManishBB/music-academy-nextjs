ce8cca81322af56f70d46c3966814e22da2fc7e8
Manish <mbb7224@gmail.com>
2024-04-10 01:38:37 +0530

    Added multiple sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +11,10 @@ export default function Home() {
         <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
             <HeroSection />
             <FeaturedCourses />
+            <WhyChooseUs />
+            <TestimonialCards />
+            <UpcomingWebinars />
+            <Instructors />
         </main>
     );
 }
